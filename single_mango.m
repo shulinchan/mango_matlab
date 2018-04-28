1ac5ac2ad689fb26e71bfd2b04bbea69c3f1e5d1
jmaeda <thecopyholder@gmail.com>
2018-04-28 13:51:47 -0400

    combine cbir into single mango script

--- single_mango.m
+++ single_mango.m
@@ -1,13 +1,25 @@
 function result = single_mango(input1,input2)
+    %SINGLE_MANGO Summary of this function goes here
+    %Detailed explanation goes here
+    
     %load images
     addpath('./images');
+    addpath('./unclassified_images');
     imgA = imread(input1);
     imgB = imread(input2);
     
-%SINGLE_MANGO Summary of this function goes here
-%   Detailed explanation goes here
+    %cbir to classify maturity
+    cbirClassification = cbirMangoClassifier(imgA,imgB);
+    
+    %stem to classify maturity
     countA = runDemo(imgA);
     countB = runDemo(imgB);
+    
+    %ripe is represented by 1, unripe is represented by 0
+    %if side-A has 0 then stem isn't visible so only check side-B, if side-B has 3-4 then it is ripe
+    %if side-B has 0 then stem isn't visible so only check side-A, if side-A has 3-4 then it is ripe
+    %if both are non 0, make sure both are within range 3-4
+    %else not ripe
     if (countA == 0 && countB < 5 && countB > 2)
         result = 1;
     elseif (countB == 0 && countA < 5 && countA > 2)
@@ -17,5 +29,8 @@ function result = single_mango(input1,input2)
     else
         result = 0;
     end
+    
+    disp(result);
+    disp(cbirClassification);
 end
 
--- testscript.m
+++ testscript.m
@@ -6,15 +6,12 @@ for i=1:nfiles
     fileName = imagefiles(i).name;
     fLen = length(fileName);
     if fileName(fLen-6) == 'A'
-        cbirMangoClassifier(fileName);
+        fileName2 = fileName;
+        fileName2(fLen-6) = 'B';
+        result = cbirMangoClassifier(fileName,fileName2);
+        
+        %print results
+        disp(fileName);
+        disp(result);
     end
 end
-
-%a = imread('Mango_08_ADM.JPG');
-%bs = imread('Mango_08_BDM.JPG');
-%figure; imshow(a);
-%b = rgb2hsv(a);
-%figure; imshow(b);
-%c = getRelevantPixels(a) * 255;
-%figure; imshow(a);
-%figure; imshow(bs);
